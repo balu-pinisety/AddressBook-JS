8a63551b316897049ec089678c209c4bef15e6d8
balu-pinisety <manikantapandu225@gmail.com>
2021-08-06 03:55:00 +0530

    Added: Contact details of class

---
+++ AddressBook.js
@@ -0,0 +1,86 @@
+console.log("Program for the Address Book");
+class Contact {
+ 
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    email;
+
+    get firstName() {
+        return this.firstName;
+    }
+    set firstName(value) {
+        this.firstName = value;
+    }
+
+    get lastName() {
+        return this.lastName;
+    }
+    set lastName(value) {
+        this.lastName = value;
+    }
+
+    get address() {
+        return this.address;
+    }
+    set address(value) {
+        this.address = value;
+    }
+
+    get city() {
+        return this.city;
+    }
+    set city(value) {
+        this.city = value;
+    }
+
+    get state() {
+        return this.state;
+    }
+    set state(value) {
+        this.state = value;
+    }
+
+    get zip() {
+        return this.zip;
+    }
+    set zip(value) {
+        this.zip = value;
+    }
+
+    get phoneNumber() {
+        return this.phoneNumber;
+    }
+    set phoneNumber(value) {
+        this.phoneNumber = value;
+    }
+
+    get email() {
+        return this.email;
+    }
+    set email(value) {
+        this.email = value;
+    }
+
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    toString() {
+        return "firstName="+this.firstName+" ,lastName="+this.lastName+
+        " ,address="+this.address+" ,city="+this.city+" ,state="+this.state+
+        " ,zip="+this.zipCode+" ,phoneNumber="+this.phoneNumber+" ,email="+this.email;
+    }
+
+}
